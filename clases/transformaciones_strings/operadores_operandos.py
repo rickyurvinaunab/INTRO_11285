14bf932db1277e9d9a811d4ca91b32d2d970860f
ricardourvina <rurvinac@estudiante.uc.cl>
2026-03-13 18:07:30 -0300

    Clase 03: transformaciones y variables

---
+++ clases/transformaciones_strings/operadores_operandos.py
@@ -0,0 +1,26 @@
+print(20 + 32)
+#52
+hour = 60
+minute = 1
+print(hour - 1)
+#59
+print(hour * 60 + minute)
+#3601
+print(minute / 60)
+#0.016666666666666666
+print(minute // 60)
+#0
+print(5 ** 2)
+#25
+print(7 % 3)
+# 1  → 7 dividido entre 3 da 2 y sobra 1
+
+
+grado=45
+pi=3.14159
+print(2 * (3-1))
+#4
+print((1+1)**(5-2))
+#8
+print(grado / 2 * pi)
+#70.68
